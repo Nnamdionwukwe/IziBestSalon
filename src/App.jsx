3d78ad4332d3b744935a6da90c711beb4e4b6553
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-02 14:25:34 +0100

    b

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 
 function App() {
+  const img = "IziBest-logo1.png";
   return (
     <>
+      <img src={img} />
       <div>I Love You Baby My Queen and More</div>
     </>
   );

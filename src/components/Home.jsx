61b152caef5ee574ad0440bbab47a4089e173a0b
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-07 22:09:29 +0100

    f

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,17 +10,18 @@ export default function Home() {
       <NavBar />
       <div>
         <img className={styles.logo} src={image} />
-      </div>
-      <div className={styles.mainDiv}>
-        <div className={styles.iziBest}>
-          <p>EXPERIENCE IZI LIFESTYLE </p>
-        </div>
 
-        <div className={styles.today}>
-          <p>
-            Today everyone need a little time away. Imagine an entire day just
-            for you... Leave refreshed and renewed!
-          </p>
+        <div className={styles.mainDiv}>
+          <div className={styles.iziBest}>
+            <p>EXPERIENCE IZI LIFESTYLE </p>
+          </div>
+
+          <div className={styles.today}>
+            <p>
+              Today everyone need a little time away. Imagine an entire day just
+              for you... Leave refreshed and renewed!
+            </p>
+          </div>
         </div>
       </div>
     </div>

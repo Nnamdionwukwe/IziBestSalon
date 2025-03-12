baaf621a3cb5c7089c4a422c7e90ed3128eae5e7
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-12 02:34:19 +0100

    s

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,24 @@ export default function Footer() {
   const image = "salon-logo.jpg";
 
   return (
-    <div className={styles.main}>
-      <div>
-        <div className={styles.Izi}>
-          <img src={image} />
-          <p>izi lifestyle</p>
-        </div>
+    <div>
+      <div className={styles.main}>
+        <div>
+          <div className={styles.Izi}>
+            <img src={image} />
+            <p>izi lifestyle</p>
+          </div>
 
-        <div className={styles.paraDiv}>
-          <h4 className={styles.para1}>Izi Lifestyle Salon</h4>
-          <h4 className={styles.para2}>Chandellux Imperial Hotel Durumi</h4>
-          <h3 className={styles.para3}>+234 803 058 8774</h3>
+          <div className={styles.paraDiv}>
+            <h4 className={styles.para1}>Izi Lifestyle Salon</h4>
+            <h4 className={styles.para2}>Chandellux Imperial Hotel Durumi</h4>
+            <h3 className={styles.para3}>+234 803 058 8774</h3>
+          </div>
         </div>
+      </div>
+
+      <div>
+        <p></p>
       </div>
     </div>
   );

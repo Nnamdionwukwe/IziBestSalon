8c00baaf499221d684d4156513eb9ce355b3344c
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-12 15:05:20 +0100

    c

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -7,14 +7,14 @@ export default function Hamburger() {
   return (
     <div className={styles.ham}>
       <div onClick={() => setIsOpen((is) => !is)}>
-        {isOpen ? (
+        {!isOpen ? (
           <i className="fa fa-bars" aria-hidden="true"></i>
         ) : (
           <i className="fa fa-times" aria-hidden="true"></i>
         )}
       </div>
 
-      {!isOpen && (
+      {isOpen && (
         <div className={styles.nav}>
           <div className={styles.navSub}>
             <div>

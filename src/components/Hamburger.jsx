27c84eff8c3fc43e9706b45fa4b51545d5f0768a
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-12 14:48:32 +0100

    g

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -5,27 +5,28 @@ export default function Hamburger() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div onClick={() => setIsOpen((is) => !is)} className={styles.ham}>
-      {!isOpen ? (
-        <>
+    <div className={styles.ham}>
+      <div onClick={() => setIsOpen((is) => !is)}>
+        {isOpen ? (
           <i className="fa fa-bars" aria-hidden="true"></i>
-        </>
-      ) : (
-        <>
+        ) : (
           <i className="fa fa-times" aria-hidden="true"></i>
-          <div className={styles.nav}>
-            <div className={styles.navSub}>
-              <div>
-                <p>Home</p>
-                <p>Services ></p>
-                <p>About ></p>
-                <p>Promotions</p>
-                <p>Lookbook</p>
-                <p>Contacts</p>
-              </div>
+        )}
+      </div>
+
+      {!isOpen && (
+        <div className={styles.nav}>
+          <div className={styles.navSub}>
+            <div>
+              <p>Home</p>
+              <p>Services ></p>
+              <p>About ></p>
+              <p>Promotions</p>
+              <p>Lookbook</p>
+              <p>Contacts</p>
             </div>
           </div>
-        </>
+        </div>
       )}
     </div>
   );

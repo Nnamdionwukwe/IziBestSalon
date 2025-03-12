ab46bcea1474e80a1e21c6c45c7a99d7f4a9c396
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-12 01:54:39 +0100

    d

--- src/components/Surburbs.jsx
+++ src/components/Surburbs.jsx
@@ -20,19 +20,19 @@ export default function Surburbs() {
 
       <div className={styles.mainPara}>
         <p>
-          Recognized as one of the premier salons on the North Shore and Lake
-          Forest neighborhood, the Tiara team is known for it's warm and
-          friendly atmosphere, helping clients experience the peace and comfort
-          they deserve. Open 6 days a week, Tiara Salon and Spa is can be found
-          just past downtown Lake Forest on Western Avenue.
+          Recognized as one of the premier salons in Abuja and in Nigeria, the
+          Izi Lifestyle team is known for it's warm and friendly atmosphere,
+          helping clients experience the peace and comfort they deserve. Open 6
+          days a week and 24/7, Izi Lifestyle Salon and Spa can be found just
+          past Apo Bridge in Chandellux Imperial Hotel Durumi.
         </p>
 
         <p>
-          At Tiara Salon, we provide to our guests the latest in salon and spa
-          treatments. From soothing manicures, pedicures, and body treatments to
-          a team of stylists that are recognized as leaders in forecasting
-          trends and developing innovative styles and color techniques - our
-          all-inclusive approach will suit your every need.
+          At Izi Lifestyle Salon, we provide to our guests the latest in salon
+          and spa treatments. From soothing manicures, pedicures, and body
+          treatments to a team of stylists that are recognized as leaders in
+          forecasting trends and developing innovative styles and color
+          techniques - our all-inclusive approach will suit your every need.
         </p>
       </div>
     </div>

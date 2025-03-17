4bfe75b8808d2ec694116565f36a50fd5c0a8fad
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-17 21:48:59 +0100

    g

--- src/components/MakeUp.jsx
+++ src/components/MakeUp.jsx
@@ -1,9 +1,18 @@
 import NavBar from "./NavBar";
+import WaxingComp from "./WaxingComp";
+import Footer from "./Footer";
+import WaxingNavigation from "./WaxingNavigation";
 
 export default function MakeUp() {
   return (
     <div>
       <NavBar />
+
+      <WaxingComp />
+
+      <WaxingNavigation />
+
+      <Footer />
     </div>
   );
 }

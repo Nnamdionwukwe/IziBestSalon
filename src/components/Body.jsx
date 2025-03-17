63ad742d3b60da7c21eb7026276ab87b680eeadc
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-17 18:41:31 +0100

    g

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,12 @@
+import BodyComp from "./BodyComp";
 import NavBar from "./NavBar";
 
 export default function Body() {
   return (
     <div>
       <NavBar />
+
+      <BodyComp />
     </div>
   );
 }

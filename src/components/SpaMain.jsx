175c4af0bd156e8b21b60c59f1866d72bed9ca98
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-18 11:25:49 +0100

    g

--- src/components/SpaMain.jsx
+++ src/components/SpaMain.jsx
@@ -4,23 +4,30 @@ export default function SpaMain() {
   const image = "salon-logo.jpg";
 
   return (
-    <div className={styles.main2}>
-      <div className={styles.mainSub}>
-        <img className={styles.logo} src={image} />
+    <div>
+      <div className={styles.main2}>
+        <div className={styles.mainSub2}>
+          <img className={styles.logo} src={image} />
 
-        <div className={styles.mainDiv}>
-          <div className={styles.iziBest2}>
-            <p>SPA PARTIES</p>
-          </div>
+          <div className={styles.mainDiv}>
+            <div className={styles.iziBest2}>
+              <p>SPA PARTIES</p>
+            </div>
 
-          <div className={styles.today}>
-            <p>
-              Discover the good life. Hit the pause button and indulge together.
-              Make any occasion<span> big, small or spontaneous,</span>
-              {""} something amazing.
-            </p>
+            <div className={styles.today2}>
+              <p>
+                Discover the good life. Hit the pause button and indulge
+                together. Make any occasion
+                <span> big, small or spontaneous,</span>
+                {""} something amazing.
+              </p>
+            </div>
           </div>
         </div>
+      </div>
+
+      <div className={styles.subMain}>
+        <h5>A Spa Party is a real treat for your loved ones.</h5>
       </div>
     </div>
   );

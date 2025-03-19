8594cbe2470d341187972d0481976cdadb0ebb70
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-19 22:04:43 +0100

    ty

--- src/components/Main3.jsx
+++ src/components/Main3.jsx
@@ -1,17 +1,11 @@
-import { useState } from "react";
 import styles from "./Main2.module.css";
-// import styles from "./Home.module.css";
 
 export default function Main3() {
-  const [email, setEmail] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-
   const image = "salon-logo.jpg";
 
   return (
     <div>
-      <div className={styles.main}>
+      <div className={styles.main3}>
         <div className={styles.mainSub}>
           <img className={styles.logo} src={image} />
 
@@ -32,39 +26,27 @@ export default function Main3() {
       </div>
 
       <div className={styles.inputMain}>
-        <div className={styles.inputDiv}>
-          <p className={styles.inputP1}>* indicates required</p>
-          <h4 className={styles.inputP2}>Email Address *</h4>
+        <div>
+          <p className={styles.location}>
+            Conveniently located at Chadellux Hotel & Suits, Durumi.
+          </p>
 
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="text"
-          />
+          <p className={styles.email}>Email: izilifestyle@gmail.com</p>
         </div>
 
-        <div className={styles.inputDiv}>
-          <h4 className={styles.inputP3}>First Name *</h4>
+        <div></div>
 
-          <input
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            type="text"
-          />
-        </div>
+        <div>
+          <h5 className={styles.add}>Address</h5>
+          <p className={styles.add2}>Chadellux Hotel & Suits, Durumi</p>
 
-        <div className={styles.inputDiv}>
-          <h4 className={styles.inputP3}>Last Name *</h4>
+          <h5 className={styles.add3}>Phones</h5>
+          <p className={styles.add2}>+234 803 0588 774 Main</p>
+          <p className={styles.add2}>+234 803 4903 356</p>
 
-          <input
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            type="text"
-          />
-        </div>
-
-        <div className={styles.subscribeDiv}>
-          <p>SUBSCRIBE</p>
+          <h5 className={styles.add3}>Hours</h5>
+          <p className={styles.mon}>Mon - Sat: 24/7</p>
+          <p className={styles.mon}>Sun: By appointment only</p>
         </div>
       </div>
     </div>

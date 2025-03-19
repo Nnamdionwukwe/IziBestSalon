e5cdb5d8315014f6294401ce0bbdbf49d29d17ab
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-19 21:11:59 +0100

    g

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,9 +1,15 @@
+import Footer from "./Footer";
+import Main2 from "./Main2";
 import NavBar from "./NavBar";
 
 export default function Contacts() {
   return (
     <h1>
       <NavBar />
+
+      <Main2 />
+
+      <Footer />
     </h1>
   );
 }

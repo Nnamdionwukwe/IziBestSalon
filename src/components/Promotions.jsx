278f3ec462dd01837bb4401abefe528150a2c0ff
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-21 22:56:42 +0100

    v

--- src/components/Promotions.jsx
+++ src/components/Promotions.jsx
@@ -1,11 +1,11 @@
-import styles from "./Main2.module.css";
+import styles from "./Main2.module.css";?
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import Main2 from "./Main2";
 
 export default function Promotions() {
   return (
-    <div className={styles.mainMain}>
+    <div>
       <NavBar />
 
       <Main2 />

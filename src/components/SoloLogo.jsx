90f45bba789ff0b375dcb99b06be26c52cd7533d
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-21 22:35:18 +0100

    hy

--- src/components/SoloLogo.jsx
+++ src/components/SoloLogo.jsx
@@ -1,13 +1,13 @@
-import styles from "./Main2.module.css";
+// import styles from "./Main2.module.css";
 
-export default function SoloLogo() {
-  const image = "salon-logo.jpg";
+// export default function SoloLogo() {
+//   const image = "salon-logo.jpg";
 
-  return (
-    <div className={styles.main}>
-      <div className={styles.mainSub}>
-        <img className={styles.logo} src={image} />
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className={styles.main}>
+//       <div className={styles.mainSub}>
+//         <img className={styles.logo} src={image} />
+//       </div>
+//     </div>
+//   );
+// }

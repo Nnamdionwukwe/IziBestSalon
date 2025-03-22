1f1c797c00ca367f49a34652e7ecba948f802206
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-22 01:32:06 +0100

    gt

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,36 @@
 import NavBar from "./NavBar";
+import styles from "./Home.module.css";
 
 export default function Products() {
   return (
-    <h1>
+    <div>
       <NavBar />
-      Products
-    </h1>
+
+      <div className={styles.main0}>
+        <div className={styles.mainSub}>
+          <div className={styles.mainDiv}>
+            <div>
+              <h4 className={styles.mainPara}>PRODUCCT SELECTION </h4>
+            </div>
+
+            <div className={styles}>
+              <p className={styles.mainPara1}>
+                At the Izi Lifestyle Salon you will find carefully selected
+                professional care products for hair, body, nails, face, and
+                skin.
+              </p>
+
+              <p className={styles.mainPara1}>
+                We've curated only the best professional products that we also
+                use on day-to-day bases. Now you can achieve same results at
+                home. Frequently we run specials on hair care and skin care
+                lines. We encourage you to often look at our promotions section,
+                subscribe to e-newsletter or visit our shop for more details.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }

2e7b0ba8291ce62fd19ba15f57fd144ae0dc9adb
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-03-22 01:40:12 +0100

    gt

--- src/components/HomeLogo.jsx
+++ src/components/HomeLogo.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Logo from "../Logo";
 import styles from "./SalonLogo.module.css";
 
@@ -5,9 +6,9 @@ export default function HomeLogo() {
   const image = "salon-logo.jpg";
 
   return (
-    <div className={styles.homeLogo}>
+    <Link to="contacts" className={styles.homeLogo}>
       <img className={styles.logo2} src={image} />
       <p>izi lifestyle</p>
-    </div>
+    </Link>
   );
 }
